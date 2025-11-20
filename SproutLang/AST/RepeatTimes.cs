c37e0ac6e55cd08afd3b948986c9f39f754524c8
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2025-11-20 21:07:36 +0100

    Functions encoder (#21)
    * Enhance function handling: support parameter declarations and improve call statement processing
    
    * Tests
    
    * Refactor loop handling: support variable expressions in repeat times and fixed binary operations
    
    * Fixed comments

--- SproutLang/AST/RepeatTimes.cs
+++ SproutLang/AST/RepeatTimes.cs
@@ -2,10 +2,10 @@ namespace SproutLang.AST;
 
 public class RepeatTimes: LoopStatement
 {
-    public IntLiteralExpression Times {get;}
+    public Expression Times {get;}
     public Block Body {get;}
     
-    public RepeatTimes(IntLiteralExpression times, Block body)
+    public RepeatTimes(Expression times, Block body)
     {
         Times = times;
         Body = body;
